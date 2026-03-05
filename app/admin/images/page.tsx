0a8a474c7b1e05e95e2f4ba29da77326e3cfccd9
Rachel Yuan <rachelyuan@rachel-air.myfiosgateway.com>
2026-03-05 18:52:40 -0500

    Fixed deployment issue

--- app/admin/images/page.tsx
+++ app/admin/images/page.tsx
@@ -2,7 +2,6 @@ import { requireSuperadmin } from "@/lib/requireSuperadmin";
 import ImagesClient from "./ImagesClient";
 
 export default async function ImagesPage() {
-  const user = await requireSuperadmin();
-
-  return <ImagesClient authedEmail={user.email ?? ""} />;
+  const user = await requireSuperadmin(); // this is a string (email)
+  return <ImagesClient authedEmail={user ?? ""} />;
 }
